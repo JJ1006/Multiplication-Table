4edc440941320f6daacda88cd9963a7dc14d9dd5
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-03-18 23:54:24 +0530

    We can have multiplication table of any number we input.
    It is made by simple python syntax.

---
+++ multiplication_table.py
@@ -0,0 +1,5 @@
+
+number = int(input("\nPlease enter the number : "))
+for i in range(1,11):
+    #print(str(number) + " X " + str(i) + " = " + str(number * i))
+    print(f"{number} X {i} = {number*i}")
